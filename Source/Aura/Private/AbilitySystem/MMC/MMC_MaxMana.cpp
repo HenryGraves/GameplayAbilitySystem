3aac0deb92d65bbbaedc2753b92e6452a2e53d9b
Henry Graves <henrygraves95@gmail.com>
2023-10-23 23:42:10 -0700

    add UI widgets for attributes interface

---
+++ Source/Aura/Private/AbilitySystem/MMC/MMC_MaxMana.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) Strange Fire Studios
+
+
+#include "AbilitySystem/MMC/MMC_MaxMana.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Intelligence = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, Intelligence);
+	Intelligence = FMath::Max<float>(Intelligence, 0.f);
+	// wow
+
+	if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject()))
+	{
+		const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+		return 60.f + 2.5f * Intelligence + 5.f * PlayerLevel;
+	}
+	return -1;
+}
